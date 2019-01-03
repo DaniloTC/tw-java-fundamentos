c2b6c60007244bd9b5d92a3671985c2e10debeaf
Danilo Castro <danilotrindade87@gmail.com>
2019-01-03 20:00:50 -0200

    Entendendo a diferença entre next() e nextLine() no scanner

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Digite o primeiro número: ");       // Entrada que retorna um "número" no tipo string.
-		//int numero1 = Integer.parseInt(scanner.nextLine()); // Converte a string recebida na linha 8 para inteiro.
-		int numero1 = scanner.nextInt();                      // Não é cast, aqui recuperamos o dado já com o tipo int.
-		System.out.println(numero1);
+//		System.out.print("Digite a mensagem1 (nextLine): ");
+//		String mensagem1 = scanner.nextLine();                    // nextLine() considera "ENTER" o fim do comando
+//		System.out.print("Digite a mensagem2 (next): ");
+//		String mensagem2 = scanner.next();                        // next() considera o primeiro espaço " " o fim do comando
+//		System.out.println("Mensagem1 (nextLine): " + mensagem1); // mensagem completa antes do "Enter"
+//		System.out.println("Mensagem2 (next)    : " + mensagem2); // mensagem completa antes do primeiro espaço
+		
+		System.out.print("Digite o primeiro número: ");       
+		//int numero1 = Integer.parseInt(scanner.nextLine()); 
+		int numero1 = scanner.nextInt();                      
+		System.out.print("Digite a operação: ");
+		char operacao = scanner.next().charAt(0); // charAt() pega um char dentro de uma string pelo indice
 		scanner.close();
 	}
 }
