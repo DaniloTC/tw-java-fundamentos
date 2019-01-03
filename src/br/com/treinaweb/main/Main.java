7239869808af6d68d4177f90f985b5e298c9dd41
Danilo Castro <danilotrindade87@gmail.com>
2019-01-02 23:04:04 -0200

    Explorando a classe Scanner

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Digite o primeiro número: ");     // Entrada que retorna um "número" no tipo string
-		int numero1 = Integer.parseInt(scanner.nextLine()); // Converte a string recebida na linha 8 para inteiro
+		System.out.print("Digite o primeiro número: ");       // Entrada que retorna um "número" no tipo string.
+		//int numero1 = Integer.parseInt(scanner.nextLine()); // Converte a string recebida na linha 8 para inteiro.
+		int numero1 = scanner.nextInt();                      // Não é cast, aqui recuperamos o dado já com o tipo int.
 		System.out.println(numero1);
 		scanner.close();
 	}
