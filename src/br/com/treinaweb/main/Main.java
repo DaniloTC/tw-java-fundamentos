73c92374bf2f080ff1a83a84438d3d7df36da117
Danilo Castro <danilotrindade87@gmail.com>
2019-01-02 22:29:59 -0200

    Entendendo o conceito de entrada com Scanner

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -1,7 +1,16 @@
 package br.com.treinaweb.main;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Olá, mundo!");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite seu Nome: ");
+		String nome = scanner.next();
+		System.out.println(nome);
+		// Desconectar a variável 'scanner' do stream de entrada
+		// do sistema para eliminar o warning da linha 7. Já que
+		// não irei mais usar a entrada de dados com Scanner.
+		scanner.close();
 	}
 }
