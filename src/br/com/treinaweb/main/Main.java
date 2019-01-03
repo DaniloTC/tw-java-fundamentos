f27b317ed7779f29c0d1f00698803cc04fb50c3d
Danilo Castro <danilotrindade87@gmail.com>
2019-01-02 22:52:23 -0200

    Entendendo o conceito de cast

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Digite seu Nome: ");
-		String nome = scanner.next();
-		System.out.println(nome);
-		// Desconectar a variável 'scanner' do stream de entrada
-		// do sistema para eliminar o warning da linha 7. Já que
-		// não irei mais usar a entrada de dados com Scanner.
+		System.out.print("Digite o primeiro número: ");     // Entrada que retorna um "número" no tipo string
+		int numero1 = Integer.parseInt(scanner.nextLine()); // Converte a string recebida na linha 8 para inteiro
+		System.out.println(numero1);
 		scanner.close();
 	}
 }
