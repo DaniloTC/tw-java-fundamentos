c868bfe9571803ad7dd5ff8f7365f5c7a5281054
Danilo Castro <danilotrindade87@gmail.com>
2019-01-04 21:50:48 -0200

    Estrutura de tomada de decisão if and else if

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -5,32 +5,31 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Digite o primeiro número: ");       
-		int numero1 = scanner.nextInt();                      
-		System.out.print("Digite a operação: ");
-		char operacao = scanner.next().charAt(0); // charAt() pega UM char dentro de uma string pelo indice
+		System.out.print("Digite o primeiro número: ");
+		int numero1 = scanner.nextInt();
+		System.out.print("Digite a operação [ +  -  *  / ]: ");
+		char operacao = scanner.next().charAt(0);
 		System.out.print("Digite o segundo número: ");
 		int numero2 = scanner.nextInt();
-		
-		/*
-		 * Para este comando funcionar String.format() foi preciso configurar o Compiler compliance seguindo
-		 * os seguintes passos:
-		 * 
-		 * Usando especificamente o Eclipse Kepler e JDK 8
-		 * 
-		 * (1) Botão direito sobre o projeto > Preferences
-		 * (2) Procurar na área lateral esquerda a opção Java Compiler
-		 * (3) Configurar na aba "JDK Compiler" a opção "Compiler compliance level" com a maior opção que achar
-		 * 
-		 * Para esses comandos funcionarem format() e printf() será necessário configurar o compiler compliance 
-		 * para uma versão superior a 1.6
-		 * Se uma mensagem de erro aparecer nessas linhas significa que o compiler compliance está configurado
-		 * em uma versão inferior a 1.6
-		 */
-		
-		// Você quer fazer a operação 9 + 2
-		System.out.println(String.format("Você quer fazer a operação %d %c %d", numero1, operacao, numero2)); // usando interpolação (visto na aula)
-		System.out.printf("Você quer fazer a operação %d %c %d", numero1, operacao, numero2);                 // usando printf (já conhecia)
+		//Se a variavel 'resultado' não for inicializada será atribuido como padrão o valor '0', mas na execução 
+		//o fluxo pode entrar no 'else' e ficar sem inicialização, com isso, o eclipse irá reclamar. Portanto, é
+		//melhor inicializar uma variável no momento da declaração.
+		int resultado = 0;
+		if (operacao == '+') {
+			resultado = numero1 + numero2; //processamento
+			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
+		} else if (operacao == '-') {
+			resultado = numero1 - numero2; //processamento
+			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
+		} else if (operacao == '*') {
+			resultado = numero1 * numero2; //processamento
+			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
+		} else if (operacao == '/') {
+			resultado = numero1 / numero2; //processamento
+			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
+		} else {
+			System.out.println("Operação inválida!"); //opção caso nenhum processamento acima seja realizado
+		}
 		scanner.close();
 	}
 }
