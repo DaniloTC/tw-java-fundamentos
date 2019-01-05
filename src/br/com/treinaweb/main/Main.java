92205237b08384a27acacad036642038ef9cea3a
Danilo Castro <danilotrindade87@gmail.com>
2019-01-05 10:22:16 -0200

    Estrutura de tomada de decisão if and if else [Editado]

--- src/br/com/treinaweb/main/Main.java
+++ src/br/com/treinaweb/main/Main.java
@@ -11,25 +11,23 @@ public class Main {
 		char operacao = scanner.next().charAt(0);
 		System.out.print("Digite o segundo número: ");
 		int numero2 = scanner.nextInt();
-		//Se a variavel 'resultado' não for inicializada será atribuido como padrão o valor '0', mas na execução 
-		//o fluxo pode entrar no 'else' e ficar sem inicialização, com isso, o eclipse irá reclamar. Portanto, é
-		//melhor inicializar uma variável no momento da declaração.
+		//Se a variavel 'resultado' não for inicializada [dentro deste escopo] o Java não irá atribuir automaticamente o
+		//valor padrão por causa de questões de escopo e 'static' e por isso na execução o fluxo pode entrar no 'else'
+		//e ficar sem inicialização, além disso, a IDE irá reclamar. Portanto, quando estiver usando variáveis dentro
+		//de um escopo específico é melhor inicializar a variável no momento da declaração.
 		int resultado = 0;
 		if (operacao == '+') {
 			resultado = numero1 + numero2; //processamento
-			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
 		} else if (operacao == '-') {
 			resultado = numero1 - numero2; //processamento
-			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
 		} else if (operacao == '*') {
 			resultado = numero1 * numero2; //processamento
-			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
 		} else if (operacao == '/') {
 			resultado = numero1 / numero2; //processamento
-			System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); //saída
 		} else {
 			System.out.println("Operação inválida!"); //opção caso nenhum processamento acima seja realizado
 		}
+		System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado)); // saída
 		scanner.close();
 	}
 }
